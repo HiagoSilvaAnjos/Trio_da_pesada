3fbc45ad7d34f5cd698b072c2b0d206e0c14990b
HiagoSilva <91165415+HiagoSilvaAnjos@users.noreply.github.com>
2025-03-06 20:27:47 +0000

    feat: adiciona a fundação do projeto

--- projeto.py
+++ projeto.py
@@ -1,2 +1,51 @@
-print("Tigrezo zap zap")
-print("tá papagando")
+#INSTALE AS DEPENDENCIAS 
+# pip install spacy
+# python -m spacy download pt_core_news_lg
+
+import spacy
+nlp = spacy.load("pt_core_news_lg")
+
+caminho_relativo_entrada = "/workspaces/Trio_da_pesada/texto_entrada.txt"
+caminho_relativo_saida = "/workspaces/Trio_da_pesada/texto_saida.txt"
+
+#HELPER FUNCTIONS
+def remover_stopwords_pontuacao(sentenca_tokenizada):  
+       return [token.text for token in nlp(sentenca_tokenizada) if not token.is_punct and not token.is_stop]
+
+#MAIN FUNCTIONS
+def abrir_arquivo(caminho_relativo):
+    try:
+        with open(caminho_relativo, "r", encoding="utf-8") as file:
+            return file.read()
+    except FileNotFoundError:
+        print("Arquivo não encontrado.")
+        return None
+    
+def processar_texto(texto):
+    doc = nlp(texto)
+    sentencas_tokenizadas = [sent.text.strip() for sent in doc.sents]
+
+    sentencas_limpas = []
+    for sentenca in sentencas_tokenizadas:
+        sentenca_limpa = remover_stopwords_pontuacao(sentenca)
+        sentencas_limpas.append(sentenca_limpa)
+
+    return (sentencas_limpas,sentencas_tokenizadas)
+
+def start(): 
+    try:
+        # Nível 1
+        texto = abrir_arquivo(caminho_relativo_entrada)
+        texto_processado = processar_texto(texto) # Vai retornar uma tupla com dois valores => (sentenças_tokenizadas, sentenças_originais)
+        print(texto_processado)
+
+        # Nível 2
+
+        #Nível 3
+    
+
+        print("Arquivo de texto processado com sucesso!")
+    except Exception as e:
+        print(f"Erro ao processar o texto: {e}")
+
+start()
